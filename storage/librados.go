282c76523b3f8bdd0cf6e899b90f9c1197b43b48
chenjiefeng <chenjiefeng@4399inc.com>
2019-02-27 19:11:48 +0800

    update

--- storage/librados.go
+++ storage/librados.go
@@ -3,6 +3,7 @@ package storage
 // 基础库
 
 /*
+#cgo LDFLAGS: -lrados
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,92 +12,92 @@ package storage
 import "C"
 
 import (
-	"errors"
 	"bytes"
 	"encoding/binary"
+	"errors"
+	"unsafe"
 )
 
 type LibRados interface {
 	Rados_version(major *int, minor *int, extra *int) string
-	Rados_create2(flags C.uint64_t) (int, error)
-	Rados_conf_read_file(path *C.char) (int, error)
-	Rados_connect() (int, error)
-	Rados_ioctx_create(pool_name *C.char, io C.rados_ioctx_t) (int, error)
-	Rados_write(key string, value string, offset uint64) (int, error)
+	Rados_create2(flags uint32) error
+	Rados_conf_read_file(path []byte) error
+	Rados_connect() error
+	Rados_ioctx_create(pool_name []byte, io C.rados_ioctx_t) error
+	Rados_write(key []byte, value []byte, offset uint) error
 	Rados_ioctx_destroy()
 	Rados_shutdown()
-	Rados_setxattr(object_name *C.char, attr_name *C.char, value string) (int, error)
-	Rados_getxattr(object_name *C.char, attr_name *C.char, size C.uint) (interface{}, error)
+	Rados_setxattr(object_name []byte, attr_name []byte, value []byte) error
+	Rados_getxattr(object_name []byte, attr_name []byte, size uint) (interface{}, error)
 }
 
 type libRados struct {
 	cluster *C.rados_t // 集群句柄
 
-	cluster_name *C.char // 集群名称
-	user_name *C.char    // 用户名
+	cluster_name []byte // 集群名称
+	user_name []byte    // 用户名
 
-	pool_name *C.char    // 对象池
+	pool_name []byte    // 对象池
 
 	io C.rados_ioctx_t
 }
 
-func NewLibRados(cluster_name string, user_name string) LibRados {
+func NewLibRados(cluster_name []byte, user_name []byte) *libRados {
 	return &libRados{
-		cluster: -1,
 		cluster_name: cluster_name,
 		user_name: user_name,
 	}
 }
 
 // 创建集群句柄
-func (lib *libRados) Rados_create2(flags uint64) (int, error) {
-	err := C.rados_create2(lib.cluster, lib.cluster_name, lib.user_name, flags)
+func (lib *libRados) Rados_create2(flags uint32) error {
+	err := C.rados_create2(lib.cluster, (*C.char)(unsafe.Pointer(&lib.cluster_name)), (*C.char)(unsafe.Pointer(&lib.user_name)), (C.ulong)(flags))
 	if err < 0 {
-		return err, errors.New("Couldn't create the ceph cluster handle! ")
+		return errors.New("Couldn't create the ceph cluster handle! ")
 	}
-	return err, nil
+	return nil
 }
 
 // 读取配置文件
-func (lib *libRados) Rados_conf_read_file(path *C.char) (int, error) {
-	err := C.rados_conf_read_file(lib.cluster, path)
+func (lib *libRados) Rados_conf_read_file(path []byte) error {
+	err := C.rados_conf_read_file(*lib.cluster, (*C.char)(unsafe.Pointer(&path)))
 	if err < 0 {
-		return err, errors.New("Cannot read config file: " + path)
+		return errors.New("Cannot read config file: " + string(path))
 	}
-	return err, nil
+	return nil
 }
 
 // 连接
-func (lib *libRados) Rados_connect() (int, error) {
-	err := C.rados_connect(lib.cluster)
+func (lib *libRados) Rados_connect() error {
+	err := C.rados_connect(*lib.cluster)
 	if err < 0 {
-		return err, errors.New("cannot connect to cluster")
+		return errors.New("cannot connect to cluster")
 	}
 
-	return err, nil
+	return nil
 }
 
 // 创建io上下文
-func (lib *libRados) Rados_ioctx_create(pool_name *C.char, io C.rados_ioctx_t) (int, error) {
+func (lib *libRados) Rados_ioctx_create(pool_name []byte, io C.rados_ioctx_t) error {
 	lib.pool_name = pool_name
-	err := C.rados_ioctx_create(lib.cluster, pool_name, &lib.io)
+	err := C.rados_ioctx_create(*lib.cluster, (*C.char)(unsafe.Pointer(&lib.pool_name)), &lib.io)
 	if err < 0 {
-		return err, errors.New("cannot open rados pool[" + lib.pool_name + "]")
+		return errors.New("cannot open rados pool[" + string(pool_name) + "]")
 	}
 
-	return err, nil
+	return nil
 }
 
 // 写入数据
-func (lib *libRados) Rados_write(key string, value string, offset uint64) (int, error) {
+func (lib *libRados) Rados_write(key []byte, value []byte, offset uint) error {
 	buf := new(bytes.Buffer)
 	_ = binary.Write(buf, binary.LittleEndian, value)
-	err := C.rados_write(lib.io, key, buf, buf.Bytes(), offset)
+	err := C.rados_write(lib.io, (*C.char)(unsafe.Pointer(&key)), (*C.char)(unsafe.Pointer(&buf)), (C.ulong)(len(buf.Bytes())), (C.size_t)(offset))
 	if err < 0 {
-		return err, errors.New("cannot write object to pool[" + lib.pool_name + "]")
+		return errors.New("cannot write object to pool[" + string(lib.pool_name) + "]")
 	}
 
-	return err, nil
+	return nil
 }
 
 // 销毁io上下文
@@ -106,25 +107,25 @@ func (lib *libRados) Rados_ioctx_destroy() {
 
 // 关闭集群句柄
 func (lib *libRados) Rados_shutdown() {
-	C.rados_shutdown(lib.cluster)
+	C.rados_shutdown(*lib.cluster)
 }
 
 // 设置属性值
-func (lib *libRados) Rados_setxattr(object_name *C.char, attr_name *C.char, value string) (int, error) {
+func (lib *libRados) Rados_setxattr(object_name []byte, attr_name []byte, value []byte) error {
 	buf := new(bytes.Buffer)
 	_ = binary.Write(buf, binary.LittleEndian, value)
-	err := C.rados_setxattr(lib.io, object_name, attr_name, buf, buf.Bytes())
+	err := C.rados_setxattr(lib.io, (*C.char)(unsafe.Pointer(&object_name)), (*C.char)(unsafe.Pointer(&attr_name)), (*C.char)(unsafe.Pointer(&buf)), (C.ulong)(len(buf.Bytes())))
 	if err < 0 {
-		return err, errors.New("cannot set extended attribute on object[" + object_name + "]")
+		return errors.New("cannot set extended attribute on object[" + string(object_name) + "]")
 	}
-	return err, nil
+	return nil
 }
 
-func (lib *libRados) Rados_getxattr(object_name *C.char, attr_name *C.char, size C.uint) (interface{}, error) {
+func (lib *libRados) Rados_getxattr(object_name []byte, attr_name []byte, size uint) (interface{}, error) {
 	buf := new(bytes.Buffer)
-	err := C.rados_getxattr(lib.io, object_name, attr_name, buf, size)
+	err := C.rados_getxattr(lib.io, (*C.char)(unsafe.Pointer(&object_name)), (*C.char)(unsafe.Pointer(&attr_name)), (*C.char)(unsafe.Pointer(&buf)), (C.size_t)(size))
 	if err < 0 {
-		return nil, errors.New("cannot get extended attribute on object[" + object_name + "]")
+		return nil, errors.New("cannot get extended attribute on object[" + string(object_name) + "]")
 	}
 	return buf, nil
 }
--- storage/librados_test.go
+++ storage/librados_test.go
@@ -3,18 +3,18 @@ package storage
 import "testing"
 
 func TestNewLibRados(t *testing.T) {
-	librados := NewLibRados("ceph", "client.admin")
-	_, err := librados.Rados_create2(0)
+	librados := NewLibRados([]byte("ceph"), []byte("client.admin"))
+	err := librados.Rados_create2(0)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	t.Log("create a cluster handle")
-	_, err = librados.Rados_conf_read_file("/etc/ceph/ceph.conf")
+	err = librados.Rados_conf_read_file([]byte("/etc/ceph/ceph.conf"))
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	t.Log("read config file")
-	_, err = librados.Rados_connect()
+	err = librados.Rados_connect()
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
